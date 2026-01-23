0022f5d850758ea526730be53afbe838c1a5264f
Wazefuk <112293501+wazefuk@users.noreply.github.com>
2026-01-22 18:02:37 -0600

    More stuff

--- script.js
+++ script.js
@@ -2,40 +2,95 @@ let presses = 0,
     ppp = 1,
     gold = 0,
     diamond = 0,
+    rainbow = 0,
     autoSpeed = 333,
-    pressesStat = document.getElementById("presses"),
+    terminate = false;
+
+let pressesStat = document.getElementById("presses"),
     pppstat = document.getElementById("perClick"),
     goldstat = document.getElementById("gold"),
     diamondstat = document.getElementById("diamond"),
-    button = document.getElementById("theButton");
+    button = document.getElementById("theButton"),
+    diamondShop = document.getElementById("diamondShop"),
+    goldShop = document.getElementById("goldShop"),
+    rainbowstat = document.getElementById("rainbow"),
+    rainbowshop = document.getElementById("rainbowShop");
 
-function refresh() {
+function refreshShops() {
+    if (goldstat.innerHTML == "Gold: 0") {
+        goldstat.style.display = "none";
+        goldShop.style.display = "none";
+    } else {
+        goldstat.style.display = "block";
+        goldShop.style.display = "block";
+    }
+
+    if (diamondstat.innerHTML == "Diamond: 0") {
+        diamondstat.style.display = "none";
+        diamondShop.style.display = "none";
+    } else {
+        diamondstat.style.display = "block";
+        diamondShop.style.display = "block";
+    }
+
+    if (rainbowstat.innerHTML == "Rainbow: 0") {
+        rainbowstat.style.display = "none";
+        rainbowshop.style.display = "none";
+    } else {
+        rainbowstat.style.display = "block";
+        rainbowshop.style.display = "block";
+    }
+}
+
+function refreshMisc() {
+    button.style.rotate = (presses % 360) + "deg";
+}
+
+function refreshCurrency() {
     pressesStat.innerHTML = "Presses: " + presses;
     pppstat.innerHTML = "Presses per Press: " + ppp;
-    button.style.rotate = (presses % 360) + "deg";
-
     goldstat.innerHTML = "Gold: " + gold;
     diamondstat.innerHTML = "Diamond: " + diamond;
-
-    if (goldstat.innerHTML == "Gold: 0")
-        goldstat.style.display = "none";
-    else
-        goldstat.style.display = "block";
-
-    if (diamondstat.innerHTML == "Diamond: 0")
-        diamondstat.style.display = "none";
-    else
-        diamondstat.style.display = "block";
+    rainbowstat.innerHTML = "Rainbow: " + rainbow;
 }
+
+// refreshes all
+function refresh() {
+    refreshShops();
+    refreshCurrency();
+    refreshMisc();
+}
+
+refresh();
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
 let shop = {
+    autocount: 0,
+    autopressescount: 0,
+    autopressescount_5: 0,
+    autogoldcount: 0,
+    autoautocount: 0,
+    autoautopressescount: 0,
+    p1pppcount: 0,
+    p5pppcount: 0,
+    goldcount: 0,
+    fasterautoscount: 0,
+    diamondcount: 0,
+    PTGcount: 0,
+    GTPcount: 0,
+    autodiamondcount: 0,
+    rainbowCount: 0,
+    p1pppprice: 180,
 
     P1ppp: function () {
-        if (presses >= 180) {
-            presses -= 180;
+        if (presses >= this.p1pppprice) {
+            presses -= this.p1pppprice;
             ppp++;
+            this.p1pppcount++;
+            this.p1pppprice = Math.round(this.p1pppprice * 1.15);
+            document.getElementById("p1pppcounter").innerHTML = this.p1pppcount;
+            document.getElementById("p1pppprice").innerHTML = '(' + this.p1pppprice + ')';
             refresh();
         }
     },
@@ -44,6 +99,8 @@ let shop = {
         if (presses >= 500) {
             presses -= 500;
             ppp += 5;
+            this.p5pppcount++;
+            document.getElementById("p5pppcounter").innerHTML = this.p5pppcount;
             refresh();
         }
     },
@@ -52,9 +109,13 @@ let shop = {
         if (presses >= 30000) {
             presses -= 30000;
             refresh();
+            this.autocount++;
+            document.getElementById("autocounter").innerHTML = this.autocount;
+            if (this.autocount > 1) return;
             while (true) {
+                if (terminate) break;
                 await sleep(autoSpeed);
-                presses += ppp;
+                presses += (ppp * this.autocount);
                 refresh();
             }
         }
@@ -64,7 +125,9 @@ let shop = {
         if (presses >= 440000) {
             presses -= 440000;
             gold++;
+            this.goldcount++;
             refresh();
+            document.getElementById("buygoldcounter").innerHTML = this.goldcount;
         }
     },
 
@@ -72,9 +135,15 @@ let shop = {
         if (gold >= 3) {
             gold -= 3;
             refresh();
+            this.autopressescount++;
+            document.getElementById("autobuypress1counter").innerHTML = this.autopressescount;
+            if (this.autopressescount > 1) return;
             while (true) {
+                if (terminate) break;
                 await sleep(autoSpeed);
-                this.P1ppp();
+                for (let i = 0; i < this.autopressescount; i++)
+                    this.P1ppp();
+                refresh();
             }
         }
     },
@@ -83,9 +152,15 @@ let shop = {
         if (gold >= 13) {
             gold -= 13;
             refresh();
+            this.autopressescount_5++;
+            document.getElementById("autobuypress5counter").innerHTML = this.autopressescount_5;
+            if (this.autopressescount_5 > 1) return;
             while (true) {
+                if (terminate) break;
                 await sleep(autoSpeed);
-                this.P5ppp();
+                for (let i = 0; i < this.autopressescount_5; i++)
+                    this.P5ppp();
+                refresh();
             }
         }
     },
@@ -94,9 +169,14 @@ let shop = {
         if (gold >= 50) {
             gold -= 50;
             refresh();
+            this.autoautocount++;
+            document.getElementById("autobuyautocounter").innerHTML = this.autoautocount;
+            if (this.autoautocount > 1) return;
             while (true) {
+                if (terminate) break;
                 await sleep(autoSpeed);
-                this.auto();
+                for (let i = 0; i < this.autoautocount; i++)
+                    this.auto();
             }
         }
     },
@@ -104,10 +184,15 @@ let shop = {
     autoBuyGold: async function () {
         if (gold >= 100) {
             gold -= 100;
-            refresh()
+            this.autogoldcount++;
+            document.getElementById("autobuygoldcounter").innerHTML = this.autogoldcount;
+            refresh();
+            if (this.autogoldcount > 1) return;
             while (true) {
+                if (terminate) break;
                 await sleep(autoSpeed);
-                this.getGold();
+                for (let i = 0; i < this.autogoldcount; i++)
+                    this.getGold();
             }
         }
     },
@@ -120,6 +205,8 @@ let shop = {
         if (presses >= 20e9) {
             presses -= 20e9;
             autoSpeed = Math.trunc((autoSpeed / 2) * 1.3);
+            this.fasterautoscount++;
+            document.getElementById("fasterautoscounter").innerHTML = this.fasterautoscount;
             refresh();
         }
     },
@@ -128,6 +215,8 @@ let shop = {
         if (gold >= 50000) {
             gold -= 50000;
             diamond++;
+            this.diamondcount++;
+            document.getElementById("1diamondcounter").innerHTML = this.diamondcount;
             refresh();
         }
     },
@@ -137,6 +226,8 @@ let shop = {
             diamond -= 4;
             presses += (gold * 440000);
             gold = 0;
+            this.GTPcount++;
+            document.getElementById("GTPcounter").innerHTML = this.GTPcount;
             refresh();
         }
     },
@@ -146,6 +237,8 @@ let shop = {
             diamond -= 4;
             gold += (Math.trunc(presses / 440000));
             presses = 0;
+            this.PTGcount++;
+            document.getElementById("PTGcounter").innerHTML = this.PTGcount;
             refresh();
         }
     },
@@ -154,15 +247,96 @@ let shop = {
         if (diamond >= 10) {
             diamond -= 10;
             refresh();
+            this.autoautopressescount++;
+            document.getElementById("autoautobuypresscounter").innerHTML = this.autoautopressescount;
+            if (this.autoautopressescount > 1) return;
             while (true) {
+                if (terminate) break;
                 await sleep(autoSpeed);
-                this.autoBuyPresses_5();
+                for (let i = 0; i < this.autoautopressescount; i++)
+                    this.autoBuyPresses_5();
             }
         }
+    },
+
+    autoBuyDiamond: async function () {
+        if (diamond >= 100) {
+            diamond -= 100;
+            this.autodiamondcount++;
+            document.getElementById("autobuydiamondcounter").innerHTML = this.autodiamondcount;
+            refresh();
+            if (this.autodiamondcount > 1) return;
+            while (true) {
+                if (terminate) break;
+                await sleep(autoSpeed);
+                for (let i = 0; i < this.autodiamondcount; i++)
+                    this.getDiamond();
+            }
+        }
+    },
+
+    buyRainbow: function () {
+        if (diamond >= 13000) {
+            diamond -= 13000;
+            rainbow++;
+            this.rainbowCount++;
+            document.getElementById("buyrainbowcounter").innerHTML = this.rainbowCount;
+            refresh();
+        }
+    },
+
+    funnel: function () {
+        if (rainbow >= 30) {
+            this.PTG();
+            while (gold >= 50000)
+                this.getDiamond();
+            if (diamond >= 13000) {
+                rainbow += Math.trunc(diamond / 13000);
+                diamond = 0;
+            }
+            refresh();
+        }
     }
-}
+};
+
+let menu = {
+    isActive: false,
+
+    menuToggle: function () {
+        this.isActive = !this.isActive;
+        let menuelement = document.getElementById("menu");
+
+        if (this.isActive)
+            menuelement.style.display = "block";
+        else
+            menuelement.style.display = "none";
+    },
+
+    terminate: async function () {
+        terminate = true;
+        await sleep(500);
+        terminate = false;
+    }
+};
 
 window.addEventListener('keydown', function(e) {
+    if (e.key === ' ')
+        e.preventDefault();
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.key == ' ') {
+        presses += ppp;
+        refresh();
+    }
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.key == 'Shift')
+        menu.menuToggle();
+});
+
+document.addEventListener('keydown', function(e) {
     switch (e.key) {
         case '1':
             shop.P1ppp();
@@ -174,34 +348,25 @@ window.addEventListener('keydown', function(e) {
             shop.auto();
             break;
         case '4':
-            shop.autoBuyPresses();
-            break;
-        case '5':
-            shop.autoBuyPresses_5();
-            break;
-        case '6':
-            shop.autoBuyAuto();
-            break;
-        case '7':
             shop.getGold();
             break;
+        case '5':
+            shop.autoBuyPresses();
+            break;
+        case '6':
+            shop.autoBuyPresses_5();
+            break;
+        case '7':
+            shop.autoBuyAuto();
+            break;
         case '8':
+            shop.autoBuyGold();
+            break;
+        case '9':
             shop.getDiamond();
             break;
     }
 });
 
-window.addEventListener('keydown', function(e) {
-    if (e.key === ' ') {
-        e.preventDefault();
-    }
-});
 
-document.addEventListener('keyup', function (event) {
-    if (event.key = "space") {
-        presses += ppp;
-        refresh();
-    }
-})
 
-refresh();
